12c941d966c526c93d1834488a795fee04d16eda
Aidas Domasevicius <aidas_dom@gmx.com>
2024-12-15 17:23:27 +1100

    PerfectSoldierMovement 1.1.0 - Add coop support

--- Lionheart-PerfectSoldierMovement/extensions/entities/units/movement_behaviours/player_movement_behaviour.gd
+++ Lionheart-PerfectSoldierMovement/extensions/entities/units/movement_behaviours/player_movement_behaviour.gd
@@ -1,12 +1,21 @@
 extends "res://entities/units/movement_behaviors/player_movement_behavior.gd"
 
 
-func get_movement()->Vector2:
-	if not RunData.is_coop_run and not RunData.get_player_effect_bool("can_attack_while_moving", 0) and not ProgressData.is_manual_aim(0):
-		var player = $"/root/Main"._players[0]
+func get_player_index() -> int:
+	if RunData.is_coop_run:
+		for i in range(CoopService.connected_players.size()):
+			if CoopService.connected_players[i][0] == device:
+				return i
+	return 0
+
+
+func get_movement() -> Vector2:
+	var player_index := get_player_index()
+	if not RunData.get_player_effect_bool("can_attack_while_moving", player_index) and not ProgressData.is_manual_aim(player_index):
+		var player = $"/root/Main"._players[player_index]
 		for weapon in player.current_weapons:
 			if (weapon._current_cooldown == 0 and
-                weapon._current_target.size() > 0 and
+				weapon._current_target.size() > 0 and
 				is_instance_valid(weapon._current_target[0]) and
 				Utils.is_between(weapon._current_target[1], weapon.current_stats.min_range, weapon.get_max_range())):
 
--- Lionheart-PerfectSoldierMovement/mod_main.gd
+++ Lionheart-PerfectSoldierMovement/mod_main.gd
@@ -1,8 +1,6 @@
 extends Node
 
 
-func _init():
+func _init() -> void:
 	var dir = ModLoaderMod.get_unpacked_dir() + "Lionheart-PerfectSoldierMovement/extensions/"
-
-	ModLoaderMod.install_script_extension(dir + "weapons/weapon.gd")
 	ModLoaderMod.install_script_extension(dir + "entities/units/movement_behaviours/player_movement_behaviour.gd")
