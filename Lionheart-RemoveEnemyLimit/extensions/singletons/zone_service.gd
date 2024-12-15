12c7f49ef03b22b194f02825ade9be6dd48fc112
Aidas Domasevicius <aidas_dom@gmx.com>
2024-12-15 17:23:46 +1100

    RemoveEnemyLimit code cleanup

--- Lionheart-RemoveEnemyLimit/extensions/singletons/zone_service.gd
+++ Lionheart-RemoveEnemyLimit/extensions/singletons/zone_service.gd
@@ -1,8 +1,7 @@
 extends "res://singletons/zone_service.gd"
 
 
-func get_wave_data(my_id: int, index: int)->Resource:
+func get_wave_data(my_id: int, index: int) -> Resource:
     var wave = .get_wave_data(my_id, index)
     wave.max_enemies += 1000000
     return wave
-
--- Lionheart-RemoveEnemyLimit/mod_main.gd
+++ Lionheart-RemoveEnemyLimit/mod_main.gd
@@ -1,8 +1,7 @@
 extends Node
 
 
-func _init():
+func _init() -> void:
 	var dir = ModLoaderMod.get_unpacked_dir() + "Lionheart-RemoveEnemyLimit/extensions/"
-	
-	ModLoaderMod.install_script_extension(dir + "singletons/zone_service.gd")
 
+	ModLoaderMod.install_script_extension(dir + "singletons/zone_service.gd")
